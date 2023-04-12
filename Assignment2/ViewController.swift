73d2dbdbdddbbbf1906e1b5a4d549ab7c9b62482
Tom <tpbenn250@gmail.com>
2023-04-12 09:26:37 +0100

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Thomas Bennett (Student) on 12/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
